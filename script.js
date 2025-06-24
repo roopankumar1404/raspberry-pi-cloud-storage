e227b8dd37d76fe16f250f6012de27974469c1fd
roopankumar1404 <roopankumar1442006@gmail.com>
2025-06-24 20:33:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.querySelectorAll('.cmd-list li').forEach(item => {
-  item.addEventListener('click', () => {
-    navigator.clipboard.writeText(item.textContent);
-    item.textContent += " ✅";
-    item.style.color = "green";
-    setTimeout(() => {
-      item.style.color = "";
-      item.textContent = item.textContent.replace(" ✅", "");
-    }, 1200);
-  });
-});
+
